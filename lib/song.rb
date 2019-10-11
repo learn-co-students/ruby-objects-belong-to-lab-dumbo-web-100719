9b7793f015d92783dda454158c66bac57c9bc8f8
RubenV-Dev <r.will16@hotmail.com>
2019-10-11 14:38:06 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,11 @@
 class Song
-    attr_accessor :title
+    attr_accessor :title, :artist
 
-    def initialize(title)
+    def initialize
         @title = title
     end
 
-    def artist(entered_name)
-        new_song = Artist.new(entered_name)
-    end
+    # def artist(entered_name)
+    #     new_song = Artist.new(entered_name)
+    # end
 end
